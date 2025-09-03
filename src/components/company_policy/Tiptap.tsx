f593768eff1d89fefa6a2962ca45e29ac19cff09
maxthenobody <hjy6296@gmail.com>
2025-09-03 15:11:53 +0900

    20250903_1511

--- src/components/company_policy/Tiptap.tsx
+++ src/components/company_policy/Tiptap.tsx
@@ -1,24 +1,27 @@
-import { useEditor, EditorContent, EditorContext } from '@tiptap/react'
-import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus'
+import { useEditor, EditorContent, EditorContext, Editor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { useMemo } from 'react'
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [StarterKit], // define your extension array
-    content: '<p>Hello World!</p>', // initial content
-  })
+    const editor = useEditor({
+        extensions: [StarterKit], // define your extension array
+        content: '<p>Hello World!</p>', // initial content
+    })
 
-  // Memoize the provider value to avoid unnecessary re-renders
-  const providerValue = useMemo(() => ({ editor }), [editor])
+    // Memoize the provider value to avoid unnecessary re-renders
+    const providerValue = useMemo(() => ({ editor }), [editor])
 
-  return (
-    <EditorContext.Provider value={providerValue}>
-      <EditorContent editor={editor} />
-      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
-    </EditorContext.Provider>
-  )
+    return (
+        <EditorContext.Provider value={providerValue}>
+            <div className="prose prose-lg max-w-none w-[1280px] h-[500px] border border-gray-300 overflow-hidden">
+                <EditorContent editor={editor} className="h-full [&_.ProseMirror]:h-full [&_.ProseMirror]:overflow-y-auto [&_.ProseMirror]:p-4 [&_.ProseMirror]:outline-none" />
+            </div>
+        </EditorContext.Provider>
+    )
 }
+
+new Editor({
+  extensions: [StarterKit],
+})
 
 export default Tiptap
